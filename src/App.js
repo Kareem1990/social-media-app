2839a8c8436299a022c01f3391c16460e89b5186
Kareem Rizk <kareem.magdy5@gmail.com>
2022-06-13 22:59:03 -0400

    Post and Postlist components work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
-import Login from "./components/Login"
-import Header from "./components/Header"
-import CreatePost from "./components/CreatePost"
+import Login from "./components/Login";
+import Header from "./components/Header";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
 
 function App() {
-  const [user, setUser] = React.useState('Reed');
+  const [user, setUser] = React.useState('');
   const [posts, setPosts] = React.useState([]);
 
   React.useEffect(() => {
     document.title = user ? `${user}'s feed` : "Please login"
   }, [user]);
+
+  function handleAddPost (newPost) {
+    setPosts([newPost, ...posts])
+  }
 
   if (!user) {
     return <Login setUser={setUser} />
@@ -18,21 +23,8 @@ function App() {
   return (
     <>
       <Header user={user} setUser={setUser} />
-      <CreatePost user={user} setPost={setPosts} posts={posts} />
-      {posts.map((post, i) => (
-      <React.Fragment key={i}>
-        {post.image && (
-          <img
-            style={{ height: 100, width: 200, objectFit: 'cover' }}
-            src={URL.createObjectURL(post.image)}
-            alt="Post Cover"
-          />
-
-        )}
-        <p>{post.content}</p>
-        <div>{user}</div>
-      </React.Fragment>
-      ))}
+      <CreatePost user={user} handleAddPost={handleAddPost} />
+      <PostList posts={posts}/>
     </>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-function Post () {
-    return <div>post</div>
+function Post ({ image, content, user }) {
 
+    return <>
+      {image && (
+        <img
+          style={{ height: 100, width: 200, objectFit: 'cover' }}
+          src={URL.createObjectURL(image)}
+          alt="Post Cover"
+        />
 
-
-
+      )}
+      <p>{content}</p>
+      <div>{user}</div>
+    </>
 }
 
 export default Post;
